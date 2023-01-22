6005617500d6dae6c5b3034cef2d8aa08b5509e0
bertyn99 <bboulikou@gmail.com>
2023-01-22 22:35:48 +0100

    improve(player): add style

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -3,9 +3,9 @@ import styled from "styled-components";
 import { ReactComponent as Play } from "../../assets/images/icon-play.svg";
 export const Player = () => {
   return (
-    <div>
-      <Play />
-    </div>
+    <PlayerButton>
+      <PlayerIcon fill={"blue"} />
+    </PlayerButton>
   );
 };
 
@@ -13,4 +13,17 @@ const PlayerButton = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
+  border-radius: 3rem;
+  border: none;
+`;
+
+const PlayerIcon = styled(Play)`
+  &:hover {
+    & circle {
+      opacity: 1;
+    }
+    & path {
+      fill: white;
+    }
+  }
 `;
