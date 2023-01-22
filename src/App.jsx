d9b4b3b920dda4422748f4ef6f635326d47a0d47
bertyn99 <bboulikou@gmail.com>
2023-01-22 22:11:21 +0100

    feat(player): add player

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import styled from "styled-components";
+import { Player } from "./components/Player/Player";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -30,6 +31,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <Player />
     </div>
   );
 }
--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,5 +1,16 @@
 import React from "react";
-
+import styled from "styled-components";
+import { ReactComponent as Play } from "../../assets/images/icon-play.svg";
 export const Player = () => {
-  return <div>Player</div>;
+  return (
+    <div>
+      <Play />
+    </div>
+  );
 };
+
+const PlayerButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
